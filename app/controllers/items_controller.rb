3efdceb7687ba10a993b2751de22cb63be51654c
mylrella1102 <tmk12.3u@gmail.com>
2023-05-22 23:12:04 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,13 @@ class ItemsController < ApplicationController
   end
 
   def new
-    
+    @item = Item.new
   end
+
+  def create
+    Item.create(item_params)
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :info, :price, :image).merge(user_id: current_user.id)
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,5 +8,6 @@ class Item < ApplicationRecord
 
   belongs_to :user
   # has_one    :purchase
-
+  
+  has_one_attached :image
 end
