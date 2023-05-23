decbbcb28d2d7c271edbc853b51cafb177914bc1
mylrella1102 <tmk12.3u@gmail.com>
2023-05-23 12:46:01 +0900

    ActiveHashの編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,18 +1,19 @@
 class Item < ApplicationRecord
-  class Article < ApplicationRecord
-    extend ActiveHash::Associations::ActiveRecordExtensions
-    belongs_to :category
-    belongs_to :sales_status
-    belongs_to :fee_status
-    belongs_to :prefecture
-  end
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
 
+  validates :image, presence: true
   validates :name, presence: true
   validates :info, presence: true
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :sales_status_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :fee_status_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price, presence: true
 
   belongs_to :user
