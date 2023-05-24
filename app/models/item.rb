54c1133760b68ee8283df1c45c2cdf7562033bce
mylrella1102 <tmk12.3u@gmail.com>
2023-05-24 12:03:02 +0900

    ダミーデータの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,15 +9,16 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :name, presence: true
   validates :info, presence: true
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :sales_status_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :fee_status_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price, presence: true, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, only_integer: true}
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :sales_status_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :fee_status_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :scheduled_delivery_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, presence: true,
+                    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true }
 
   belongs_to :user
   # has_one    :purchase
-  
+
   has_one_attached :image
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -69,25 +69,25 @@ RSpec.describe Item, type: :model do
       it 'priceが300以下では保存できない' do
         @item.price = '299'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
 
       it 'priceが9,999,999より高いと保存できない' do
         @item.price = '10000000'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
 
       it 'priceが半角数字以外の値が含まれていると保存できない' do
         @item.price = 'aaaaa'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not a number")
+        expect(@item.errors.full_messages).to include('Price is not a number')
       end
 
       it 'userが紐付いていないと保存できない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("User must exist")
+        expect(@item.errors.full_messages).to include('User must exist')
       end
     end
   end
