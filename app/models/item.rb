398572f31902d726bafe3941d33b82b41978e1ce
mylrella1102 <tmk12.3u@gmail.com>
2023-05-24 11:02:50 +0900

    商品一覧の表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-  belongs_to :sales_status
-  belongs_to :fee_status
-  belongs_to :prefecture
-  belongs_to :scheduled_delivery
+  belongs_to_active_hash :category
+  belongs_to_active_hash :sales_status
+  belongs_to_active_hash :fee_status
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :scheduled_delivery
 
   validates :image, presence: true
   validates :name, presence: true
