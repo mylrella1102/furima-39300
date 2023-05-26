5966b005d4fd213d5b4c0afbab39460da1b2f638
mylrella1102 <tmk12.3u@gmail.com>
2023-05-26 23:10:07 +0900

    クレジット決済機能の実装

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :token, :postal_code, :prefecture_id, :city, :house_number, :building, :phone_number, :item_id, :user_id
+  attr_accessor :token, :postal_code, :prefecture_id, :city, :house_number, :building, :phone_number, :item_id, :user_id, :token
 
   validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   validates :city, presence: true
@@ -9,6 +9,7 @@ class OrderAddress
   validates :item_id, presence: true
   validates :user_id, presence: true
   validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :token, presence: true
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :order do
-    
+    postal_code
+    prefecture_id
+    city
+    house_number
+    building
+    phone_number
+
+    token {"tok_abcdefghijk00000000000000000"}
   end
 end
